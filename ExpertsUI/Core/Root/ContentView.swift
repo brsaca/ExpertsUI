bbe79a4ed7829155c27ce08e501ff3d794f5557f
brsaca <brsaca@gmail.com>
2023-09-27 13:02:37 -0600

    style: add topbar

--- ExpertsUI/Core/Root/ContentView.swift
+++ ExpertsUI/Core/Root/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .top) {
+            // MARK: Background
+            Color.calendarBkg
+                .ignoresSafeArea()
+            
+            // MARK: Top Bar
+            HStack(alignment: .top) {
+                Image("bren")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                    .padding(.trailing, 10)
+                
+                Group {
+                    Text("Hey")
+                        .foregroundColor(.gray)
+                        .fontWeight(.light)
+                    
+                    Text("Bren!")
+                        .foregroundColor(.black)
+                        .fontWeight(.regular)
+                }
+                .font(.title2)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.down")
+                    .frame(height: 20)
+                    .fontWeight(.semibold)
+            }
+            .padding(.horizontal, 24)
+            
         }
-        .padding()
     }
 }
 
--- ExpertsUI/Extensions/Color.swift
+++ ExpertsUI/Extensions/Color.swift
@@ -8,6 +8,6 @@
 import SwiftUI
 
 extension Color {
-    static let calendarBkg = LinearGradient(colors: [Color("calendarBkg"), Color("bottomBkg")], startPoint: .topLeading, endPoint: .bottomTrailing)
-    static let dayBkg = LinearGradient(colors: [Color("dayBkg"), Color("bottomBkg")], startPoint: .topLeading, endPoint: .bottomTrailing)
+    static let calendarBkg = LinearGradient(colors: [Color("calendarBkg"), Color("bottomBkg")], startPoint: .top, endPoint: .bottom)
+    static let dayBkg = LinearGradient(colors: [Color("dayBkg"), Color("bottomBkg")], startPoint: .top, endPoint: .bottom)
 }
