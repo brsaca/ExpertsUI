21fc51e835476433b814c802d0562dda61960b44
brsaca <brsaca@gmail.com>
2023-09-27 12:14:20 -0600

    Initial Commit

---
+++ ExpertsUI/ExpertsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpertsUIApp.swift
+//  ExpertsUI
+//
+//  Created by Brenda Saavedra Cantu on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpertsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
